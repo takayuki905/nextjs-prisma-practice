7acce861f8910d6d8ed8eeef98c4c45ce8597bea
Takayuki Kumagai <xtakayukikumagai@gmail.com>
2026-01-12 09:42:55 +0900

    cleanup seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,13 +1,7 @@
 import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-
 async function main() {
-  // FYI: https://stackoverflow.com/questions/79845075/prismaclientinitializationerror-prismaclient-needs-to-be-constructed-with-a-n
-  const adapter = new PrismaBetterSqlite3({
-    url: process.env.DATABASE_URL || "file:./prisma/dev.db",
-  });
-  const prisma = new PrismaClient({ adapter });
+  const prisma = new PrismaClient();
 
   const posts = [
     {
